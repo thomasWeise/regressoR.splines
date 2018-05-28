f50e518c2d17189873c1cb68f9b532f285501a07
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-28 17:11:41 +0800

    Fixed Wrong Parameter

--- R/smoothSpline.R
+++ R/smoothSpline.R
@@ -28,12 +28,8 @@
 #' @param transformation.y the transformation along the \code{y}-axis, or
 #'   \code{NULL} if none was applied to the data
 #' @param metric.transformed the transformed metric for the first fitting step
-#' @param forceEnd should the function be extrapolated beyond the right-most
-#'   point on the \code{x}-axis (\code{FALSE}) or simply stick at the last
-#'   \code{y} value (\code{TRUE})?
-#' @param forceStart should the function be extrapolated beyond the left-most
-#'   point on the \code{x}-axis (\code{FALSE}) or simply stick at the first
-#'   \code{y} value (\code{TRUE})?
+#' @param protected should the function be limited to the range of values
+#'   actually ocurring in the data?
 #' @param ... parameters to be passed to \code{\link[stats]{smooth.spline}}
 #' @return On success, an instance of \code{\link{FittedSplineModel}},
 #'   \code{NULL} on failure.
@@ -42,10 +38,10 @@
 regressoR.spline.smooth <- function(metric,
                                     transformation.x=NULL, transformation.y=NULL,
                                     metric.transformed=NULL,
-                                    forceEnd=TRUE, forceStart=TRUE,
+                                    protected=TRUE,
                                     ...) {
   .fitSpline(metric, .smooth.splineFitter, transformation.x,
              transformation.y, metric.transformed,
-             forceEnd=forceEnd, forceStart=forceStart,
+             protected=protected,
              ...);
 }
